8526bd62a418294c7bad8a302dd0c499d40742d1
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-18 23:39:47 +0300

    new update

--- src/Navigation/NavBarData.js
+++ src/Navigation/NavBarData.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import EventIcon from '@material-ui/icons/Event';
+
 
 
 export const NavBarData = [
     {
         title:"Home",
         link:"#"
+
     },
+
     {
         title:"About Us",
-       
         link:"#"
     },
     {
         title:"Event",
-        icon:<EventIcon/>,
         link:"#"
     },
     {
         title:"Contact",
-        icon:"#",
         link:"#"
     },
 ]
