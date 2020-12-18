b116e5d1d281eca8e006855fc0fee7d3a86eaf30
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-18 13:17:45 +0300

    Navbar changes

---
+++ src/Navigation/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            <div className="main-header">
+             <div className="header-upper">
+             
+             </div>
+            <div className="header-lower">
+            
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
