385610d511fadf64fcfe30a8185f375181d95615
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-21 22:56:01 +0300

    event page update

---
+++ src/Pages/EventPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class EventPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EventPage
