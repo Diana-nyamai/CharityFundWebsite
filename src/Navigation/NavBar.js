895e8ddc3c5e82956034be44594af9ebbb98f756
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-22 19:58:49 +0300

    navbar change

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -1,32 +1,9 @@
 import React from 'react'
-import { NavBarData } from './NavBarData'
 
-
-function NavBar() {
+const NavBar = () => {
     return (
         <div>
-            <div className="main-header">
-             <div className="header-upper">
-              <ul className="upper-ul">
-              <li className="row">Call us:+254712 345 678</li>
-              <li className="row">Email us:support@gmail.com</li>
-              </ul>
-             </div>
-            <div className="header-lower">
-              <ul className="nav-list">
-              {
-                  NavBarData.map((value, key) =>{
-                      return(
-                          <li key={key} className="row" onClick={() => window.location.pathname= value.link}>
-                          <div id="title"> {value.title}</div>
-                          </li>
-                      )
-                  })
-              }
-              </ul>
-                
-            </div>
-            </div>
+            
         </div>
     )
 }
