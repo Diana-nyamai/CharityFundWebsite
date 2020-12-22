971de9a2249e663c2b0cda9af239eadacf730289
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-22 20:36:44 +0300

    navbar changes

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -3,11 +3,15 @@ import React from 'react'
 const NavBar = () => {
     return (
        <Nav>
+       <NavLink to="/">
+          <h1>LOGO</h1>
+       </NavLink>
+       <Bars/>
        <NavMenu>
-       <NavLink>Home</NavLink>
-       <NavLink>About us</NavLink>
-       <NavLink>Event</NavLink>
-       <NavLink>Contact Us</NavLink>
+       <NavLink to="/">Home</NavLink>
+       <NavLink to="/AboutPage">About us</NavLink>
+       <NavLink to="/EventPage">Event</NavLink>
+       <NavLink to="ContactPage">Contact </NavLink>
        </NavMenu>
        </Nav>
     )
