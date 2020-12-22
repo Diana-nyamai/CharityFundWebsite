287d3fb3e1f57c8e26bd37248d584258c382bc86
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-22 22:51:20 +0300

    navbarelements changes

--- src/Navigation/NavBarElements.js
+++ src/Navigation/NavBarElements.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 import {NavLink as Link} from 'react-router-dom';
 import {FaBars} from 'react-icons';
+
+export const Nav = styled.nav`
+  background:pink;
+  height:80px;
+  display:flex;
+  justify-content:space-between;
+  padding:0.5rem calc(100vw-1000x)/2;
+  z-index:10;
+`
+export const NavLink = styled(Link)`
+  color:#fff;
+  display:flex;
+  padding:0 1;
+  height:100%;
+  align-items:centre;
+  text-decoration:none;
+  cursor:pointer;
+  
+
+`
