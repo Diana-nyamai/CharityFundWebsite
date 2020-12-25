d06193253450aaeb8ad16b84932c2e3bb50a16c4
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-25 18:38:34 +0300

    about page changes

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -4,7 +4,7 @@ export class AboutPage extends Component {
     render() {
         return (
             <div>
-                <h1>this is the about page</h1>
+                <h1>This is the about page</h1>
             </div>
         )
     }
