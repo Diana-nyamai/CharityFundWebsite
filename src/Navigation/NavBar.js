2701bd361a72a3a9aef65ce65981c27f4e30cfad
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-25 18:36:33 +0300

    navbar.js update

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Nav, NavLink } from './NavBarElements'
-
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const NavBar = () => {
     return (
-       <Nav>
+       <Router> <Nav>
        <NavLink to="/">
           <h1>LOGO</h1>
        </NavLink>
      
        
        </Nav>
+       </Router>
+      
     )
 }
 
