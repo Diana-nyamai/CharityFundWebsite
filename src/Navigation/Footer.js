0b940c3ad6001c85ef8701bf8a77712e7065e401
Diana nyamai <dnyamai.dn@gmail.com>
2020-12-27 16:21:17 +0300

    Update Footer.js

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -7,8 +7,12 @@ import {BrowserRouter as Router} from 'react-router-dom';
 const Footer = () => {
 
     return (
-
-<div><h1>This is the footer</h1><div>
+<Router>
+<div>
+        <h1>This is the footer</h1>
+        
+        <div>
+        </Router>
 ) 
 } 
 
