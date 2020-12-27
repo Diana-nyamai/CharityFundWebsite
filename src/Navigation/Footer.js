7af93c5e7f4614a4529a39d03e804d3913e6837c
Diana nyamai <dnyamai.dn@gmail.com>
2020-12-27 16:18:25 +0300

    Update Footer.js

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
     return (
 
 <div><h1>This is the footer</h1><div>
+) 
+} 
 
-
-
-
+export default Footer
 
 
 
@@ -28,8 +28,8 @@ const Footer = () => {
 
       
 
-)
 
-}
 
-export default Footer
+
+
+
