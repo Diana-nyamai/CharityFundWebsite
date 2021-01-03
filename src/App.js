56bfeae9fc88512a1c6c201266e2e1bb7b71ba45
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-03 16:08:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import NavBar from './Navigation/NavBar';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import EventPage from './Pages/EventPage';
+import HomePage from './Pages/HomePage';
 function App() {
   return (
+    <Router>
     <div className="App">
-     <NavBar/>
+    <Route path="/" component={HomePage}/>
+     <Route path="/EventPage" component={EventPage}/>
     </div>
+    
+    </Router>
   );
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,70 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export class HomePage extends Component {
-    render() {
-        return (
-            <div>
-                <h1>This is the homepage</h1>
-            </div>
-        )
-    }
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Router>
+            <MDBNavbar color="pink" fixed="top" dark expand="md" scrolling transparent>
+              <MDBNavbarBrand href="/">
+                <strong>Navbar</strong>
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/EventPage">Event</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/AboutPage">About Us</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/ContactPage">Contact Us</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </Router>
+
+          <MDBView src="https://images.unsplash.com/photo-1459183885421-5cc683b8dbba?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8YmxhY2slMjBjaGFyaXR5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+            <MDBMask overlay="purple-light" className="flex-center flex-column text-white text-center">
+              <h2>This Navbar is fixed</h2>
+              <h5>It will always stay visible on the top, even when you scroll down</h5>
+              <p>Navbar's background will switch from transparent to solid color while scrolling down</p><br />
+              <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+            </MDBMask>
+          </MDBView>
+        </header>
+
+        <main>
+          <MDBContainer className="text-center my-5">
+            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </MDBContainer>
+        </main>
+      </div>
+    );
+  }
 }
 
-export default HomePage
+export default Homepage;
