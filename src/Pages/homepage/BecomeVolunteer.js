c23e14ed1201b7b5f8232633229976a4a8abf4ee
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-04 19:28:46 +0300

    becaomevolunteer update

--- src/Pages/homepage/BecomeVolunteer.js
+++ src/Pages/homepage/BecomeVolunteer.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 function BecomeVolunteer() {
     return (
-        <div>
-        <h1 style={{textAlign:'center'}}>Become A Volunteer</h1>
+        <div className="volunteer">
+        <h1 style={{textAlign:'center' ,color:'#fff'}}>Become A Volunteer</h1>
            
             <p style={{textAlign:'center'}}>So seed seed green that winged cattle in. Gathering thing made 
             fly you're divided deep leave on the medicene moved us land years living.</p>
+            
         </div>
     )
 }
