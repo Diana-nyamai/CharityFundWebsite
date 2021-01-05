8e7a3041a92fc684ccf7b88793e006239cda1f13
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-05 14:11:27 +0300

    contact us update

--- src/Pages/contactpage/ContactPage.js
+++ src/Pages/contactpage/ContactPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { MDBNavbar, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBContainer, MDBMask, MDBView } from 'mdbreact';
+import { MDBNavbar, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem,MDBMask, MDBView } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Nav,Navbar} from 'react-bootstrap';
+import ContactUs from './ContactUs';
 
 class ContactPage extends React.Component {
   constructor(props) {
@@ -56,9 +57,7 @@ class ContactPage extends React.Component {
         </header>
 
         <main>
-          <MDBContainer className="text-center my-5">
-            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </MDBContainer>
+         <ContactUs/>
         </main>
       </div>
     );
---
+++ src/Pages/contactpage/ContactUs.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBInput } from "mdbreact";
+import EmailIcon from '@material-ui/icons/Email';
+import PersonIcon from '@material-ui/icons/Person';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+
+export class ContactUs extends Component {
+    render() {
+        return (
+            <section className="my-5">
+      <MDBRow>
+        <MDBCol lg="5" className="lg-0 mb-4">
+          <MDBCard>
+            <MDBCardBody>
+              <div className="form-header blue accent-1">
+                <h3 className="mt-2">
+                  <EmailIcon/> Write to us:
+                </h3>
+              </div>
+              <p className="dark-grey-text">
+                We will write back within 24hrs
+              </p>
+              <div className="md-form">
+                <MDBInput
+                  label="Your name"
+                  iconClass="grey-text"
+                  type="text"
+                  id="form-name"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  label="Your email"
+                  iconClass="grey-text"
+                  type="text"
+                  id="form-email"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  label="Subject"
+                  iconClass="grey-text"
+                  type="text"
+                  id="form-subject"
+                />
+              </div>
+              
+              <div className="text-center">
+                <MDBBtn color="pink">Submit</MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="7">
+          <div
+            id="map-container"
+            className="rounded z-depth-1-half map-container"
+            style={{ height: "400px" }}
+          >
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.6338605984747!2d36.73452031470259!3d-1.3961496361495398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f05cbfac861ff%3A0x680b30b9f4644fd1!2sMaasai%20Lodge%20Rongai!5e0!3m2!1sen!2ske!4v1605682258469!5m2!1sen!2ske"
+              title="This is a unique title"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              style={{ border: 0 }}
+            />
+          </div>
+          <br />
+          <MDBRow className="text-center">
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="map-marker-alt" />
+              </MDBBtn>
+              <p>Nairobi</p>
+              <p className="mb-md-0">Kenya</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="phone" />
+              </MDBBtn>
+              <p>0101170937</p>
+              <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="envelope" />
+              </MDBBtn>
+              <p>info@gikosh.com</p>
+              <p className="mb-md-0">sale@gikosh.com</p>
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+      </MDBRow>
+    </section>
+        )
+    }
+}
+
+export default ContactUs
