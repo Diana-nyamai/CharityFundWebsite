878792792d76c9861d5a8708a7355d823ca2ed87
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-05 13:38:16 +0300

    update

--- src/Pages/homepage/Volunteers.js
+++ src/Pages/homepage/Volunteers.js
@@ -12,22 +12,22 @@ export class Volunteers extends Component {
                 <div className="col-lg-3 col-md-6">
                 <div className="team-item">
                 <div className="team-img">
-                <img className="img-fluid" src="../Images/sly.jpeg" alt="C1"/>
+                <img className="img-fluid" src="../Images/wairimu.jpeg" alt="C1"/>
                 <div className="team-name">
                   <h4>Wairimu</h4>
                   <p>Manager</p>
                   <p className="mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   <div className="social">
-                  <i className="icons"><a href="!#"><FacebookIcon/></a></i>
-                  <i className="icons"><a href="!#"><InstagramIcon/></a></i>
-                  <i className="icons"><a href="!#"><LinkedInIcon/></a></i>
-                 <i className="icons"><a href="!#"><TwitterIcon/></a></i>
+                  <i className="icons"><FacebookIcon/></i>
+                  <i className="icons"><InstagramIcon/></i>
+                  <i className="icons"><LinkedInIcon/></i>
+                 <i className="icons"><TwitterIcon/></i>
                   </div> </div> </div></div></div>
 
                   <div className="col-lg-3 col-md-6">
                 <div className="team-item">
                 <div className="team-img">
-                <img className="img-fluid" src="../Images/alex2.png" alt="C1"/>
+                <img className="img-fluid" src="../Images/alex.png" alt="C1"/>
                 <div className="team-name">
                   <h4>Alex Kamanga</h4>
                   <p>Volunteer</p>
@@ -42,7 +42,7 @@ export class Volunteers extends Component {
                   <div className="col-lg-3 col-md-6">
                 <div className="team-item">
                 <div className="team-img">
-                <img className="img-fluid" src="../Images/sly.jpeg" alt="C1"/>
+                <img className="img-fluid" src="../Images/sly.png" alt="C1"/>
                 <div className="team-name">
                   <h4>Sylviah Mwitumi</h4>
                   <p>Volunteer</p>
@@ -57,9 +57,9 @@ export class Volunteers extends Component {
                   <div className="col-lg-3 col-md-6">
                 <div className="team-item">
                 <div className="team-img">
-                <img className="img-fluid" src="../Images/alex2.png" alt="C1"/>
+                <img className="img-fluid" src="../Images/lillian.png" alt="C1"/>
                 <div className="team-name">
-                  <h4>Alex</h4>
+                  <h4>Lillian Wachira</h4>
                   <p>Volunteer</p>
                   <p className="mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   <div className="social">
