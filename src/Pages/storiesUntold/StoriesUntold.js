b4919f7d8d35cdef4fe69c5c7b2054fced08c72e
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-06 17:49:37 +0300

    stories untold

--- src/Pages/storiesUntold/StoriesUntold.js
+++ src/Pages/storiesUntold/StoriesUntold.js
@@ -3,7 +3,7 @@ import { MDBNavbar, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDB
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Nav,Navbar} from 'react-bootstrap';
 import {Animated} from "react-animated-css";
-import AllVolunteers from './AllVolunteers';
+
 
 class StoriesUntold extends React.Component {
   constructor(props) {
@@ -39,10 +39,10 @@ class StoriesUntold extends React.Component {
                   <MDBNavItem>
                     <Nav.Link href="/EventPage">Events</Nav.Link>
                   </MDBNavItem>
-                  <MDBNavItem>
+                  <MDBNavItem active>
                   <Nav.Link href="/StoriesUntold">Stories Untold</Nav.Link>
                 </MDBNavItem>
-                  <MDBNavItem active>
+                  <MDBNavItem >
                     <Nav.Link href="/AboutPage">About Us</Nav.Link>
                   </MDBNavItem>
                   <MDBNavItem>
@@ -54,7 +54,7 @@ class StoriesUntold extends React.Component {
           </Router>
           <MDBView src="https://images.unsplash.com/photo-1504159506876-f8338247a14a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjBjaGFyaXR5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
             <MDBMask overlay="purple-light" className="flex-center flex-column text-white text-center">
-            <Animated animationIn="fadeInDownBig"> <h2 style={{fontSize:'100px'}}>About Us</h2></Animated>
+            <Animated animationIn="fadeInDownBig"> <h2 style={{fontSize:'100px'}}>STORIES UNTOLD</h2></Animated>
               
             </MDBMask>
           </MDBView>
@@ -65,12 +65,8 @@ class StoriesUntold extends React.Component {
             <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </MDBContainer>
 
-          <div style={{paddingBottom:'2em'}}>
-       <h1 style={{textAlign:'center'}}>Meet Our Volunteers and Team</h1>
-       <p style={{textAlign:'center'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-       </div>
+          
 
-       <AllVolunteers/>
         </main>
       </div>
     );
