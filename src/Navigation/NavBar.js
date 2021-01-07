5de42085da2af326322b9e28c69ff6a7e6261d5c
Diana nyamai <dnyamai.dn@gmail.com>
2021-01-07 19:20:41 +0300

    Update NavBar.js

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -1,58 +1,59 @@
-import React from 'react';
-import { MDBNavbar, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
-import {Nav,Navbar} from 'react-bootstrap';
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapse: false,
-      isWideEnough: false,
-    };
-    this.onClick = this.onClick.bind(this);
-  }
 
-  onClick() {
-    this.setState({
-      collapse: !this.state.collapse,
-    });
-  }
 
-  render() {
-    return (
-      <div>
-        <header>
-          <Router>
-            <MDBNavbar color="pink" fixed="top" dark expand="md" scrolling transparent>
-              <Navbar.Brand href="/">
-                <strong>Navbar</strong>
-              </Navbar.Brand>
-              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
-              <MDBCollapse isOpen={this.state.collapse} navbar>
-                <MDBNavbarNav right>
-                  <MDBNavItem active>
-                    <MDBNavLink to="/">Home</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <Nav.Link href="/EventPage">Event</Nav.Link>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="/AboutPage">About Us</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="/ContactPage">Contact Us</MDBNavLink>
-                  </MDBNavItem>
-                </MDBNavbarNav>
-              </MDBCollapse>
-            </MDBNavbar>
-          </Router>
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+  
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           
-        </header>
+
 
      
 
    
-  }
-}
+ 
+
 
