45fd182670c0aca51aa65298f0e9b2230142b377
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-09 13:18:16 +0300

    footer update

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -7,6 +7,8 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+
 
 const Footer= () => {
   return (
