7c168f569240f9545272ecc285a302afb0be6c80
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-13 14:25:06 +0300

    update

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -63,7 +63,7 @@ const Footer= () => {
             <p className="text-center text-md-left grey-text">
               &copy; {new Date().getFullYear()} Copyright:{" "}
                All rights reserved|
-               website designed by <a href="">Diana nyamai</a>
+               website designed by <a href="/EventPage">Diana nyamai</a>
             </p>
           </MDBCol>
           <MDBCol md="4" lg="4" className="ml-lg-0">
