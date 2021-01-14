97367a35218f8a8c2096c03d3050da76e8b4b9d1
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-14 12:44:29 +0300

    update

--- src/Pages/homepage/LatestNews.js
+++ src/Pages/homepage/LatestNews.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import TodayIcon from '@material-ui/icons/Today';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
 
 function LatestNews() {
     return (
@@ -12,12 +14,51 @@ function LatestNews() {
       </div>
       <div className="blog-item-text">
       <div className="date">
-         january 15, 2021
+        <TodayIcon/> january 15, 2021
       </div>
-        <p><a href=""  style={{color:'black'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></p>
+        <p><a href=""  style={{color:'black'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Phasellus at nibh efficitur, vulputate sapien vehicula, iaculis lorem
+        </a></p>
+        <div className="date">
+         <AccessTimeIcon/> 5 mins read
+        </div></div></div> </div>
+      
+        <div className="col-lg-4 col-md-6 col-xs-12 blog-item">
+      <div  className="blog-item-wrapper">
+      <div className="blog-item-img">
+      <a href="">
+      <img style={{borderRadius:'5px'}} className="img-fluid w-100" src="../Images/C1.jpg" alt="blog"/>
+      </a>
       </div>
+      <div className="blog-item-text">
+      <div className="date">
+        <TodayIcon/> january 15, 2021
       </div>
+        <p><a href=""  style={{color:'black'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Phasellus at nibh efficitur, vulputate sapien vehicula, iaculis lorem
+        </a></p>
+        <div className="date">
+         <AccessTimeIcon/> 5 mins read
+        </div></div></div> </div>
+
+        <div className="col-lg-4 col-md-6 col-xs-12 blog-item">
+      <div  className="blog-item-wrapper">
+      <div className="blog-item-img">
+      <a href="">
+      <img style={{borderRadius:'5px'}} className="img-fluid w-100" src="../Images/C1.jpg" alt="blog"/>
+      </a>
       </div>
+      <div className="blog-item-text">
+      <div className="date">
+        <TodayIcon/> january 15, 2021
+      </div>
+        <p><a href=""  style={{color:'black'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Phasellus at nibh efficitur, vulputate sapien vehicula, iaculis lorem
+        </a></p>
+        <div className="date">
+         <AccessTimeIcon/> 5 mins read
+        </div></div></div> </div>
+     
       
       </div>
     )
