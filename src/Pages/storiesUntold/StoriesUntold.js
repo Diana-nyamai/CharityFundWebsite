8cdbb4e0ab239e22dabc14581825872528b9ade6
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-17 18:24:58 +0300

    storiesuntold update

--- src/Pages/storiesUntold/StoriesUntold.js
+++ src/Pages/storiesUntold/StoriesUntold.js
@@ -3,6 +3,7 @@ import { MDBNavbar, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDB
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Nav,Navbar} from 'react-bootstrap';
 import {Animated} from "react-animated-css";
+import LatestNews from '../homepage/LatestNews';
 
 
 class StoriesUntold extends React.Component {
@@ -61,12 +62,7 @@ class StoriesUntold extends React.Component {
         </header>
 
         <main>
-          <MDBContainer className="text-center my-5">
-            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </MDBContainer>
-
-          
-
+          <LatestNews/> 
         </main>
       </div>
     );
