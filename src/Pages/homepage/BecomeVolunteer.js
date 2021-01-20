420b3e01bbaf5c7d99fb9f24552f152bbd8357b5
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-20 13:40:23 +0300

    becomevolunteer update

--- src/Pages/homepage/BecomeVolunteer.js
+++ src/Pages/homepage/BecomeVolunteer.js
@@ -4,7 +4,7 @@ import React from 'react'
 function BecomeVolunteer() {
     return (
         <div className="volunteer">
-       
+       <div style={{padding:'40px 10px'}}>
            <h1 style={{textAlign:'center'}}>Become A Volunteer</h1>
             <p style={{textAlign:'center',color:'#000'}}  className="h-100">
             
@@ -12,7 +12,7 @@ function BecomeVolunteer() {
             fly you're divided deep leave on the medicene moved us land years living.
             <p className="button"><a href="https://docs.google.com/forms/d/e/1FAIpQLScElLmk-MuBzgNdWiY2LCdJ8W_kGeaseegRp4xT_GGGFRtylQ/viewform?usp=pp_url">JOIN US TODAY</a></p>
             </p>
-           
+           </div>
         </div>
     )
 }
