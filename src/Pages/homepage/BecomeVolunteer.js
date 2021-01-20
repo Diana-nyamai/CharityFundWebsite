f2abcdcfb67563af0f6403aaa58d03108a94bc00
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-20 10:14:24 +0300

    update

--- src/Pages/homepage/BecomeVolunteer.js
+++ src/Pages/homepage/BecomeVolunteer.js
@@ -1,11 +1,14 @@
+import zIndex from '@material-ui/core/styles/zIndex'
 import React from 'react'
 
 function BecomeVolunteer() {
     return (
         <div className="volunteer">
-        <h1 style={{textAlign:'center' ,color:'#000',display:'flex'}} className="h-100">Become A Volunteer</h1>
-           
-            <p style={{textAlign:'center',color:'#000'}}  className="h-100">So seed seed green that winged cattle in. Gathering thing made 
+       
+           <h1 style={{textAlign:'center'}}>Become A Volunteer</h1>
+            <p style={{textAlign:'center',color:'#000'}}  className="h-100">
+            
+            So seed seed green that winged cattle in. Gathering thing made 
             fly you're divided deep leave on the medicene moved us land years living.
             <p className="button"><a href="https://docs.google.com/forms/d/e/1FAIpQLScElLmk-MuBzgNdWiY2LCdJ8W_kGeaseegRp4xT_GGGFRtylQ/viewform?usp=pp_url">JOIN US TODAY</a></p>
             </p>
