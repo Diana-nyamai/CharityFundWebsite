2e4f08d55d59a1928e9f9effe996a553bf0e91eb
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-24 21:26:35 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'animate.css/animate.css'
 import '../src/css/Homepage.css'
-import {HashRouter, Link,Router} from 'react-router-dom';
+import {HashRouter as Router,Route} from 'react-router-dom';
 import EventPage from './Pages/eventpage/EventPage';
 import Homepage from './Pages/homepage/HomePage';
 import ContactPage from './Pages/contactpage/ContactPage';
@@ -12,9 +12,11 @@ function App() {
   return (
     <Router>
     <div className="App">
-    <HashRouter basename="/"/>
-  
-    <HashRouter basename="/AboutPage" component={AboutPage}/>
+    <Route exact path="/" component={Homepage}/>
+     <Route path="/EventPage" component={EventPage}/>
+     <Route path="/StoriesUntold" component={StoriesUntold}/>
+     <Route path="/ContactPage" component={ContactPage}/>
+     <Route path="/AboutPage" component={AboutPage}/>
     <Footer/>
     </div></Router>
   );
